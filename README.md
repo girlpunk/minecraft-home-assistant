a5b964d79ce45fe26b999dd8b8f343a1325741ab
Jacob Mansfield <jacob@jacobmansfield.co.uk>
2020-04-21 13:58:53 +0100

    Initial release. Fixes MAC address issue and SRV record update problems.

---
+++ README.md
@@ -0,0 +1,28 @@
+# Minecraft Updated
+
+This is a copy of Home Assistant's built-in Minecraft server sensor, with some bugs and limitations fixed.
+
+Current operation should be relatively similar to the built-in integration, though the IDs generated will be different.
+
+Minecraft servers allow players to play the sandbox video game Minecraft by Mojang AB online or via a local area network with other players.
+The Minecraft Server integration lets you retrieve information from a Minecraft server (Java edition) within Home Assistant.
+
+##### The server must be version 1.7 or higher, since older versions don't expose any information.
+
+##Configuration via the frontend
+
+In the settings go to Integrations, click on the + sign to add an integration and click on "Minecraft Server (Updated)".
+After completing the configuration flow, the Minecraft Server integration will be available.
+Entity IDs will be generated based on the name you enter for the server.
+
+## Binary sensors
+This integration provides a binary sensor for the following information from a Minecraft server:
+- Connection status
+ 
+## Sensors
+This integration provides sensors for the following information from a Minecraft server:
+- Latency time
+- Version
+- Protocol version
+- Number of online players (player names are available in state attributes)
+- Number of maximum players
---
+++ custom_components/minecraft_updated/config_flow.py
@@ -0,0 +1,92 @@
+"""Config flow for Minecraft Server integration."""
+import voluptuous as vol
+
+from homeassistant import config_entries
+from homeassistant.config_entries import ConfigFlow
+from homeassistant.const import CONF_HOST, CONF_NAME, CONF_PORT
+
+from . import MinecraftServer, helpers
+from .const import (  # pylint: disable=unused-import
+    DEFAULT_HOST,
+    DEFAULT_NAME,
+    DEFAULT_PORT,
+    DOMAIN,
+)
+
+
+class MinecraftServerConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Handle a config flow for Minecraft Server."""
+
+    VERSION = 1
+    CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_POLL
+
+    async def async_step_user(self, user_input=None):
+        """Handle the initial step."""
+        errors = {}
+
+        if user_input is not None:
+            port = DEFAULT_PORT
+            # Split address at last occurrence of ':'.
+            address_left, separator, address_right = user_input[CONF_HOST].rpartition(
+                ":"
+            )
+            # If no separator is found, 'rpartition' return ('', '', original_string).
+            if separator == "":
+                host = address_right
+            else:
+                host = address_left
+                try:
+                    port = int(address_right)
+                except ValueError:
+                    pass  # 'port' is already set to default value.
+
+            # Validate port configuration (limit to user and dynamic port range).
+            if (port < 1024) or (port > 65535):
+                errors["base"] = "invalid_port"
+            # Validate host and port by checking the server connection.
+            else:
+                # Create server instance with configuration data and ping the server.
+                config_data = {
+                    CONF_NAME: user_input[CONF_NAME],
+                    CONF_HOST: host,
+                    CONF_PORT: port,
+                }
+                server = MinecraftServer(self.hass, "dummy_unique_id", config_data)
+                await server.async_check_connection()
+                if not server.online:
+                    # Host or port invalid or server not reachable.
+                    errors["base"] = "cannot_connect"
+                else:
+                    # Build unique_id and config entry title.
+                    title = user_input[CONF_NAME]
+
+                    # Abort in case the host was already configured before.
+                    await self.async_set_unique_id(user_input[CONF_NAME])
+                    self._abort_if_unique_id_configured()
+
+                    # Configuration data are available and no error was detected, create configuration entry.
+                    return self.async_create_entry(title=title, data=config_data)
+
+        # Show configuration form (default form in case of no user_input,
+        # form filled with user_input and eventually with errors otherwise).
+        return self._show_config_form(user_input, errors)
+
+    def _show_config_form(self, user_input=None, errors=None):
+        """Show the setup form to the user."""
+        if user_input is None:
+            user_input = {}
+
+        return self.async_show_form(
+            step_id="user",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        CONF_NAME, default=user_input.get(CONF_NAME, DEFAULT_NAME)
+                    ): str,
+                    vol.Required(
+                        CONF_HOST, default=user_input.get(CONF_HOST, DEFAULT_HOST)
+                    ): vol.All(str, vol.Lower),
+                }
+            ),
+            errors=errors,
+        )
